4083a9e5681823550bfde32762a54673f8153ff4
SnowOwl <mknoll1901@gmail.com>
2022-08-15 13:15:44 +0200

    [add] Discord Button

---
+++ components/DiscordButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <button
+      class="inline-block cursor-pointer transition transition-duration-900 text-dark-800 dark:text-light-900 hover:( rotate-720 text-hex-5865F2 )"
+      i-fa6-brands-discord
+      data-bs-toggle="modal"
+      data-bs-target="#discord"
+    />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
