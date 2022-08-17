38cf5fb50b20fe7991798020b8d18b1a60818fa9
SnowOwl <mknoll1901@gmail.com>
2022-08-17 10:28:58 +0200

    [add] test ThemeSwitcher component

---
+++ components/ThemeSwitcher.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button @click="switchTheme()" class="counter-btn">switchTheme: {{ mode }}</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const mode = useColorMode({
+  attribute: 'theme',
+  modes: {
+    // custom colors
+    update: 'update',
+    default: 'default',
+  },
+})
+
+const switchTheme = () => {
+  if (mode.value == 'update') {
+    mode.value = 'default'
+  } else mode.value = 'update'
+}
+
+mode.value = 'default'
+</script>
