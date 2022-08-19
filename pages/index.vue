75e2de618c65b91a0e9525f726afb8e8ac35282f
SnowOwl <mknoll1901@gmail.com>
2022-08-19 11:06:42 +0200

    [update] index page- test dynamic component change

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,35 @@
 <template>
   <div>
-    <Hero />
-
-    <section class="mt-10 flex justify-center">
-      <Counter />
-    </section>
+    <!-- <Hero /> -->
+    <img src="/img/gaming-website-template.jpg" alt="" width="1920" height="700" />
+    <main class="bg-base text-center">
+      <button @click="changeComp()">my Button</button>
+      <component :is="asyncComponent" />
+      <h1 class="text-5xl text-center m-0 pt-8rem h-1000px text-white">News</h1>
+    </main>
   </div>
 </template>
 
 <script setup>
-definePageMeta({
-  pageTransition: {
-    name: 'page',
-    mode: 'out-in',
-    appear: true,
-  },
+const template = ref('default')
+const asyncComponent = shallowRef(null)
+
+function setAsyncComponentValue() {
+  asyncComponent.value = defineAsyncComponent(() => {
+    return import(`~/components/${template.value}/Tester.vue`) // works
+  })
+}
+
+function changeComp() {
+  if (template.value === 'default') {
+    template.value = 'testComp'
+  } else template.value = 'default'
+
+  setAsyncComponentValue()
+}
+
+onMounted(() => {
+  setAsyncComponentValue()
 })
 </script>
 
