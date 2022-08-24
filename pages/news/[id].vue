c9bc1e4588ecfe1c79fbeb7d8dde49a86748d54b
SnowOwl <mknoll1901@gmail.com>
2022-08-24 14:10:43 +0200

    [add] dynamic news page

---
+++ pages/news/[id].vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="mt-200px flex flex-col justify-center items-center">
+    <NuxtLink to="/" class="text-white text-3xl mb-20">
+      <div i-fa6-solid-angles-left />
+      Home
+    </NuxtLink>
+    <p class="text-2xl text-white m-0">Route params: {{ $route.params.id }}</p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
