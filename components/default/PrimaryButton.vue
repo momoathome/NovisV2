416448a03405cf7e42368e2eb35a0b678976eb91
SnowOwl <mknoll1901@gmail.com>
2022-08-24 14:11:11 +0200

    [update] primaryButton with link and link prop

--- components/default/PrimaryButton.vue
+++ components/default/PrimaryButton.vue
@@ -1,22 +1,28 @@
 <template>
-  <button
-    type="button"
-    class="cursor-pointer font-sans tracking-1px uppercase font-bold relative inline-block z-0 px-4 py-2 text-xs whitespace-nowrap md:( px-8 py-3 text-sm ) xl:( px-12 py-4 ) rounded-lg shadow-xl border-none bg-transparent text-white transition-all transition-duration-300 before:( content-empty absolute z--1 inset-0 px-2px py-2px transition-all rounded-lg )"
+  <NuxtLink
+    :to="to"
+    class="btn cursor-pointer no-underline font-sans tracking-1px uppercase font-bold relative inline-block z-0 px-4 py-2 text-xs whitespace-nowrap md:( px-8 py-3 text-sm ) xl:( px-12 py-4 ) rounded-lg shadow-xl border-none bg-transparent text-white transition-all transition-duration-300 before:( content-empty absolute z--1 inset-0 px-2px py-2px transition-all rounded-lg )"
   >
     <slot />
-  </button>
+  </NuxtLink>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps({
+  to: {
+    type: String,
+  },
+})
+</script>
 
 <style scoped lang="scss">
-button:hover {
+.btn:hover {
   &::after {
     opacity: 1;
   }
 }
 
-button::before {
+.btn::before {
   background: var(--background-gradient);
   -webkit-mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
   mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
@@ -24,7 +30,7 @@ button::before {
   mask-composite: exclude;
 }
 
-button::after {
+.btn::after {
   content: '';
   top: 0;
   left: 0;
