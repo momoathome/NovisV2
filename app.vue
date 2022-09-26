19d9dd486bcf060dcb5720897a80c27dfdf8ad33
SnowOwl <mknoll1901@gmail.com>
2022-09-26 21:03:52 +0200

    [feat] app.vue for default layout as parent

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <NuxtLayout :name="layout">
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script setup>
+const route = useRoute()
+console.log('route name = ' + route.fullPath)
+
+const layout = ref('default')
+
+if (route.fullPath.slice(1, 9) == 'insanity') {
+  layout.value = 'insanity'
+} else layout.value = 'default'
+
+console.log('layout value = ' + layout.value)
+</script>
