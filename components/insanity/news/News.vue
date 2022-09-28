e70a38b3032992421f8f371d95d29bb9457b09ce
SnowOwl <mknoll1901@gmail.com>
2022-09-28 21:41:01 +0200

    [feat] news img

--- components/insanity/news/News.vue
+++ components/insanity/news/News.vue
@@ -6,55 +6,100 @@
     <div class="container w-1140px m-auto pb-100px">
       <div class="grid grid-cols-2 gap-8">
         <!-- main News -->
-        <div class="bg-base rounded-10px p-10 shadow-black/20 shadow-md">
-          <div class="flex">
-            <p class="uppercase font-bold font-sans text-sm m-0">26.09.2022 at 21:00</p>
-          </div>
+        <div class="bg-base rounded-10px shadow-black/20 shadow-md">
           <div>
-            <h2 class="text-base_light uppercase m-block-4">Headline</h2>
-            <p class="font-sans text-sm leading-6 max-w-35ch">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis velit
-              accusantium architecto, accusamus placeat quibusdam quis eos ipsam ratione
-              est?
-            </p>
-            <InsanityPrimaryButton class="mt-4" :target="'_blank'">
-              Read more
-            </InsanityPrimaryButton>
+            <img
+              src="https://source.unsplash.com/random"
+              alt=""
+              class="w-full h-225px object-cover rounded-t-10px" />
+          </div>
+          <div class="pt-5 pb-10 px-10">
+            <div class="flex">
+              <div i-tabler-clock class="me-1" />
+              <p class="uppercase font-bold font-sans text-xs m-0">26.09.2022 at 21:00</p>
+            </div>
+            <div>
+              <h2 class="text-base_light uppercase m-block-2">Headline News Test</h2>
+              <p class="font-sans text-sm leading-6 max-w-55ch">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis velit
+                accusantium architecto, accusamus placeat quibusdam quis eos ipsam ratione
+                est?
+              </p>
+              <InsanityPrimaryButton class="mt-4" :target="'_blank'">
+                Read more
+              </InsanityPrimaryButton>
+            </div>
           </div>
         </div>
 
         <div class="grid gap-4">
-          <div class="bg-base rounded-10px p-4 shadow-black/20 shadow-md">
-            <div class="flex">
-              <p class="uppercase font-bold font-sans text-sm m-0">26.09.2022 at 21:00</p>
+          <div class="bg-base rounded-10px shadow-black/20 shadow-md flex">
+            <div class="max-h-152px">
+              <img
+                src="https://source.unsplash.com/random"
+                alt=""
+                class="w-150px h-full object-cover rounded-10px" />
             </div>
-            <div>
-              <h3 class="text-base_light uppercase m-block-1">Headline</h3>
-              <p class="font-sans text-sm leading-5.5 max-w-35ch m-0">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit Facilis velit
-              </p>
+            <div class="p-4">
+              <div class="flex">
+                <div i-tabler-clock class="me-1" />
+                <p class="uppercase font-bold font-sans text-xs m-0">
+                  26.09.2022 at 21:00
+                </p>
+              </div>
+              <div>
+                <h3 class="text-base_light uppercase m-block-2">Headline</h3>
+                <p class="font-sans text-sm leading-5.5 max-w-50ch">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro tempora
+                  rem corrupti inventore aperiam eos
+                </p>
+              </div>
             </div>
           </div>
-          <div class="bg-base rounded-10px p-4 shadow-black/20 shadow-md">
-            <div class="flex">
-              <p class="uppercase font-bold font-sans text-sm m-0">26.09.2022 at 21:00</p>
+          <div class="bg-base rounded-10px shadow-black/20 shadow-md flex">
+            <div class="max-h-152px">
+              <img
+                src="https://source.unsplash.com/random"
+                alt=""
+                class="w-150px h-full object-cover rounded-10px" />
             </div>
-            <div>
-              <h3 class="text-base_light uppercase m-block-1">Headline</h3>
-              <p class="font-sans text-sm leading-5.5 max-w-35ch m-0">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit Facilis velit
-              </p>
+            <div class="p-4">
+              <div class="flex">
+                <div i-tabler-clock class="me-1" />
+                <p class="uppercase font-bold font-sans text-xs m-0">
+                  26.09.2022 at 21:00
+                </p>
+              </div>
+              <div>
+                <h3 class="text-base_light uppercase m-block-2">Headline</h3>
+                <p class="font-sans text-sm leading-5.5 max-w-50ch">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro tempora
+                  rem corrupti inventore aperiam eos
+                </p>
+              </div>
             </div>
           </div>
-          <div class="bg-base rounded-10px p-4 shadow-black/20 shadow-md">
-            <div class="flex">
-              <p class="uppercase font-bold font-sans text-sm m-0">26.09.2022 at 21:00</p>
+          <div class="bg-base rounded-10px shadow-black/20 shadow-md flex">
+            <div class="max-h-152px">
+              <img
+                src="https://source.unsplash.com/random"
+                alt=""
+                class="w-150px h-full object-cover rounded-10px" />
             </div>
-            <div>
-              <h3 class="text-base_light uppercase m-block-1">Headline</h3>
-              <p class="font-sans text-sm leading-5.5 max-w-35ch m-0">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit Facilis velit
-              </p>
+            <div class="p-4">
+              <div class="flex">
+                <div i-tabler-clock class="me-1" />
+                <p class="uppercase font-bold font-sans text-xs m-0">
+                  26.09.2022 at 21:00
+                </p>
+              </div>
+              <div>
+                <h3 class="text-base_light uppercase m-block-2">Headline</h3>
+                <p class="font-sans text-sm leading-5.5 max-w-50ch">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro tempora
+                  rem corrupti inventore aperiam eos
+                </p>
+              </div>
             </div>
           </div>
         </div>
