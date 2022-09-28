ed25e3bf75a72f2303630baf45adbbac606640ad
SnowOwl <mknoll1901@gmail.com>
2022-09-28 21:41:20 +0200

    [feat] prime button color

--- components/insanity/PrimaryButton.vue
+++ components/insanity/PrimaryButton.vue
@@ -2,8 +2,7 @@
   <NuxtLink
     :to="to"
     :target="target"
-    class="cursor-pointer no-underline font-sans uppercase font-bold inline-block px-3.5 py-2 rounded-2xl bg-primary text-white shadow-black/20 shadow-lg border-none transition-all transition-duration-300 hover:bg-secondary"
-  >
+    class="cursor-pointer no-underline font-sans uppercase font-bold inline-block px-3.5 py-2 rounded-2xl bg-secondary text-white shadow-black/20 shadow-lg border-none transition-all transition-duration-300 hover:bg-secondary/70">
     <slot />
   </NuxtLink>
 </template>
